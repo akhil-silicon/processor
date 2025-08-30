e28fc720e50aa1e621ecc6399f9262f196ebe7a1
akhil-silicon <akhil.vault@gmail.com>
2025-08-31 02:20:36 +0530

    Update processor_tb.v

--- processor_tb.v
+++ processor_tb.v
@@ -8,20 +8,20 @@ integer i ;
 initial begin
     for (i=0;i<32;i=i+1)
         begin
-            proc.GPR[i]=2 ;
+            proc.GPR[i]=i*4 ;
         end
 end
 
 initial begin
 proc.IR=0;
 proc.SGPR=0;
-proc.`oper= 2;
+proc.`oper= 4;
 proc.`rdst=1;
 proc.`mode=0;
 proc.`rsrc1=3;
 proc.`rsrc2=2;
 #10 ;
-$display("OPERATION= MUL NUM1=%0d NUM2=%0d RESULT= %0d",proc.GPR[3],proc.GPR[2],proc.GPR[1]);
+$display("OPERATION= MUL NUM1=%0d NUM2=%0d RESULT= %0d",proc.GPR[3],proc.GPR[2],{proc.SGPR,proc.GPR[1]});
 $display("---------------------------------------------------------------------------------------------------------------");
 
 end
