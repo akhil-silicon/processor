389917bcf6a6773dc4fb56abe82727adebe00676
akhil-silicon <akhil.vault@gmail.com>
2025-08-30 22:23:29 +0530

    Update topmodule.v

--- topmodule.v
+++ topmodule.v
@@ -1,76 +1,84 @@
-module topmodule();
-  
-  // defining Instruction register (IR) bit segments.
-  `define oper IR[31:27];
-  `define drg IR[26:22];
-  `define srg1 IR[21:17];
-  `define mode IR[16];
-  `define srg2 IR[15:11];
-  `define imm_data IR[10:0];
+`timescale 1ns / 1ps
 
-  // defining different operations.
-  `define movsgpr 5'b00000;
-  `define mov     5'b00001;
-  `define add     5'b00010;
-  `define sub     5'b00011;
-  `define mul     5'b00100;
+`define oper IR[31:27]
+`define rdst IR[26:22]
+`define rsrc1 IR[21:17]
+`define mode IR[16]
+`define rsrc2 IR[15:11]
+`define imm_data IR[15:0]
 
-  //define general purpose registers
-  reg [15:0]GPR[31:0]; 
-
-  //define special general purpose register for multiplication.
-  reg [15:0] SGPR;
-
-  //temp variable to store 32 bit multiplication result.
-  reg [31:0] mul_temp;
-
-
-  always@(*) begin
-    case (`oper)
-      `movsgpr: begin
-        GPR[`drg]= SGPR;
-      end
-      `mov: begin
-        if(mode)
-          GPR[`drg]=`imm_data;
-        else
-          GPR[`drg]=`srg1;
-      end
-      `add: begin
-        if(mode)
-          GPR[`drg]=`srg1+`imm_data;
-        else
-          GPR[`drg]=`srg1+`srg2;
-      end
-      `sub: begin
-        if(mode)
-          GPR[`drg]=`srg1-`imm_data;
-        else
-          GPR[`drg]=`srg1-`srg2;
-      end
-      `mul: begin
-        if(mode)
-          mul_temp=`srg1*`imm_data;
-        else
-          mul_temp=`srg1*`srg2;
-
-        SGPR=mul_temp[31:16];
-        GPR[`rdrg]=mul_temp[15:0];
-      end
-    endcase
-
-    
-      
-
-    
-  end
-  
-  
+`define movsgpr 5'b00000
+`define mov 5'b00001
+`define add 5'b00010
+`define sub 5'b00011
+`define mul 5'b00100
 
 
 
 
 
+module top ();
 
-  
+reg [31:0] IR ; // Instruction register for storing 32 bits instruction.
+
+reg [15:0] GPR[31:0] ; // 16bit wide 32 general purpose registers.
+reg [15:0] SGPR ;
+reg [31:0] mul_temp ;
+
+always @ (*)
+    begin
+        case(`oper)
+        movsgpr: begin 
+            GPR[`rdst]=SGPR ;
+        end
+        
+        mov: begin
+            if(`mode)
+                begin
+                    GPR[`rdst]=`imm_data ;
+                end
+             else
+                begin
+                    GPR[`rdst]=GPR[`rsrc1] ;
+                end    
+        end
+        
+        add: begin
+            if(`mode)
+                begin
+                    GPR[`rdst]=GPR[`rsrc1] +`imm_data ;
+                end
+             else
+                begin
+                    GPR[`rdst]=GPR[`rsrc1] + GPR[`rsrc2] ;
+                end  
+        
+        end
+        
+         sub: begin
+            if(`mode)
+                begin
+                    GPR[`rdst]=GPR[`rsrc1] - `imm_data ;
+                end
+             else
+                begin
+                    GPR[`rdst]=GPR[`rsrc1] - GPR[`rsrc2] ;
+                end  
+        
+        end
+        
+        mul: begin
+            if(`mode)
+                begin
+                    {SGPR,GPR[`rdst]}=GPR[`rsrc1] * `imm_data ;
+                end
+             else
+                begin
+                    {SGPR,GPR[`rdst]}=GPR[`rsrc1] * GPR[`rsrc2] ;
+                end  
+        
+        end
+    end
+
+
 endmodule
